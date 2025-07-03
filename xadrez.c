577b5f00203fe57811566935ecdbc6b09946a6bb
volk <lucas.volkmann2001@gmail.com>
2025-07-03 06:08:27 -0300

    Adventurer level: Implements tower movement using nested loops

--- xadrez.c
+++ xadrez.c
@@ -5,30 +5,45 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    // Nível Aventureiro - Movimentação do Cavalo
     const int bishopMove = 5;
     const int towerMove = 5;
     const int queenMove = 8;
 
+    const int horseFirstStepMove = 2;
+    const int horseSecondStepMove = 1;
+
     int i;
 
-    printf("Bishop Movement\n");
+
+    printf("Movimento do bispo: \n");
     for(i = 0; i < bishopMove; i++) {
         printf("Cima Direita\n");
     }
 
-    printf("\nTower Movement\n");
+
+    printf("\nMovimento da torre:\n");
     i = 0;
     while(i++ < towerMove) {
         printf("Direita\n");
     }
 
-    printf("\nQueen Movement\n");
+
+    printf("\nMovimento da rainha: \n");
     i = 0;
     do {
         printf("Esquerda\n");
     } while(i++ < queenMove);
+
+
+    printf("\nMovimento da torre: \n");
+    i = 0;
+    for (int j = 0; j < horseFirstStepMove; j++) {
+        printf("Baixo\n");
+        while (j + 1 >= horseFirstStepMove && (i++ < horseSecondStepMove)) {
+            printf("Direita\n");
+        }
+    }
 
     return 0;
 }
